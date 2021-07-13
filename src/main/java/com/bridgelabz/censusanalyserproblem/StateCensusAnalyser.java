b1c93dd2018074d9058ff47435c123de90af40fb
adarshEr2020 <adarshprajapati31@gmail.com>
2021-07-13 15:20:15 +0530

    [Adarsh] Added welcome StateCensusAnalyser

---
+++ src/main/java/com/bridgelabz/censusanalyserproblem/StateCensusAnalyser.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.censusanalyserproblem;
+
+public class StateCensusAnalyser {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to StateCensusAnalyser :");
+
+	}
+
+}
